5476c2ab5c8931790b14eb7ce74f677ed8322ca2
etienne <et.perot@gmail.com>
2022-03-09 18:46:22 +0100

    add solution that i found online...:-(((

--- 2021/moonsnumbrellas.py
+++ 2021/moonsnumbrellas.py
@@ -3,13 +3,18 @@ sys.setrecursionlimit(10000)
 from collections import defaultdict
 
 
-def get_min_cost(seq, cj, jc):
+def get_min_cost_1(seq, cj, jc):
+    """recursion method
+
+    Note: I do not like this. It is bulky and very hard to debug.
+    It is much better to learn how to do DP using for loops!
+    """
     memory = dict() 
 
     xy = defaultdict(int)
     xy['JC'] = jc
     xy['CJ'] = cj
-    def helper(i, last=""): 
+    def f(i, last=""): 
         nonlocal memory
         nonlocal seq 
         nonlocal xy
@@ -19,22 +24,51 @@ def get_min_cost(seq, cj, jc):
 
         c = seq[i]
         k = (i,last)
-        if k in memory:
-            return memory[k]
+        l = (i,'J' if last == 'C' else 'J')
+            
+        if k not in memory:
 
         if c == '?':
-            a = helper(i+1, 'J') + xy[last+'J'] 
-            b = helper(i+1, 'C') + xy[last+'C']
+            a = f(i+1, 'J') + xy[last+'J'] 
+            b = f(i+1, 'C') + xy[last+'C']
             cost = min(a,b)
         else:
-            cost = helper(i+1, c) + xy[last+c] 
-            
+            cost = f(i+1, c) + xy[last+c] 
         
+        if l in memory:
+            cost = min(memory[k], memory[l])
+
         memory[k] = cost 
+        
+        
 
         return memory[k] 
     
-    return helper(0) 
+    min_cost = f(0) 
+    return min_cost 
+
+
+def get_min_cost_2(seq, x, y):
+    """no recursion: taken from winner..."""
+    n = len(seq)
+    dp = [[1e9 for i in range(2)] for j in range(n+1)]  
+    dp[0][0] = 0
+    dp[1][0] = 0
+    for i in range(1, n + 1):
+        for c in range(2):
+            if c == 0 and seq[i-1] == 'J': # JJ
+                continue
+            if c == 1 and seq[i-1] == 'C': # CC
+                continue
+            for d in range(2):
+                cost = 0
+                if i > 1:
+                    if(d == 0 and c == 1): # CJ
+                        cost += x
+                    if(d == 1 and c == 0): # JC
+                        cost += y
+                dp[i][c] = min(dp[i][c], dp[i-1][d] + cost)
+    return min(dp[n][0], dp[n][1])
 
 
 
@@ -54,8 +88,12 @@ def main_file(f):
     cases = read_file(f)
     for case_num, case in enumerate(cases):
         cj, jc, seq = case
-        cost = get_min_cost(seq, cj, jc)
-        print(f"Case #{case_num}: {cost}")
+        cost = get_min_cost_1(seq, cj, jc)
+        cost2 = get_min_cost_2(seq, cj, jc)
+        print(f"Case #{case_num}: {cost} {cost2}")
+        if cost != cost2:
+            import pdb;pdb.set_trace()
+            pass
 
 def main_input():
     num_cases = int(input())
@@ -64,7 +102,7 @@ def main_input():
         cj = int(s[0])
         jc = int(s[1])
         seq = s[2] 
-        cost = get_min_cost(seq, cj, jc)
+        cost = get_min_cost_1(seq, cj, jc)
         print(f"Case #{case_num}: {cost}")
     
 
